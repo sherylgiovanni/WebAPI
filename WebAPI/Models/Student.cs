ae9b84ae9ffb86f045489c187b02ab03909a9f5a
Sheryl Tania <sherylgiovanni@gmail.com>
2023-09-27 10:57:04 -1000

    Change JSON property name

--- WebAPI/Models/Student.cs
+++ WebAPI/Models/Student.cs
@@ -1,10 +1,19 @@
-﻿namespace WebAPI.Models
+﻿using System.Text.Json.Serialization;
+
+namespace WebAPI.Models
 {
     public class Student
     {
+        [JsonPropertyName("EMPLID")]
         public virtual string EMPLID { get; set; }
+
+        [JsonPropertyName("FIRST_NAME")]
         public virtual string FIRST_NAME { get; set; }
+
+        [JsonPropertyName("LAST_NAME")]
         public virtual string LAST_NAME { get; set; }
+
+        [JsonPropertyName("EMAIL_ADDRESS")]
         public virtual string EMAIL_ADDRESS { get; set; }
     }
 }
